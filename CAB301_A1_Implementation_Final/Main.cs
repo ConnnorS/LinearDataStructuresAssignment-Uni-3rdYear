c050dca0af83e0c54936c42ae2920a36681f81fa
ConnnorS <connorsouth117@protonmail.com>
2024-03-29 16:54:52 +1000

    Initial commit

---
+++ CAB301_A1_Implementation_Final/Main.cs
@@ -0,0 +1,5 @@
+class App {
+    static void Main() {
+
+    }
+}
---
+++ CAB301_A1_Implementation_Final/MemberCollection.cs
@@ -0,0 +1,179 @@
+﻿//CAB301 assessment 1 
+//The implementation of MemberCollection ADT
+using System;
+using System.Globalization;
+using System.Linq;
+
+
+//Invariants: Capacity >= Count; Count >=0; and members in this member collection are sorted in alphabetical order by their full name (if there are two members who have the same last name, they are sorted by their first name in alphabetical order).
+
+
+class MemberCollection : IMemberCollection
+{
+    // Fields
+    private int capacity;
+    private int count;
+    private Member[] members; //members are sorted in alphabetical order
+
+    // Properties
+
+    // get the capacity of this member collection 
+    // pre-condition: nil
+    // post-condition: return the capacity of this member collection and this member collection remains unchanged
+    public int Capacity { get { return capacity; } }
+
+    // get the number of members in this member collection 
+    // pre-condition: nil
+    // post-condition: return the number of members in this member collection and this member collection remains unchanged
+    public int Number { get { return count; } }
+
+    public Member[] Members { get { return members; } }
+
+
+
+    // Constructor - to create an object of member collection 
+    // Pre-condition: capacity > 0
+    // Post-condition: an object of this member collection class is created
+
+    public MemberCollection(int capacity)
+    {
+        if (capacity > 0)
+        {
+            this.capacity = capacity;
+            members = new Member[capacity];
+            count = 0;
+        }
+    }
+
+    // check if this member collection is full
+    // Pre-condition: nil
+    // Post-condition: return true if this member collection is full; otherwise return false. This member collection remains unchanged.
+    public bool IsFull()
+    {
+        return count == capacity;
+    }
+
+    // check if this member collection is empty
+    // Pre-condition: nil
+    // Post-condition: return true if this member collection is empty; otherwise return false. This member collection remains unchanged.
+    public bool IsEmpty()
+    {
+        return count == 0;
+    }
+
+    // Add a new member to this member collection
+    // Pre-condition: this member collection is not full
+    // Post-condition: The given member is added to this member collection and the members remains sorted in alphabetical order by their full names, if the given member does not appear in this member collection; otherwise, the given member is not added to this member collection. 
+    // No duplicate has been added into this the member collection
+    public void Add(IMember member)
+    {
+        // check if the collection is full
+        if (IsFull())
+        {
+            Console.WriteLine("Collection is full, member not inserted.");
+            return;
+        }
+        // if the collection is empty, don't bother with sorting
+        if (IsEmpty())
+        {
+            members[0] = (Member)member;
+            count++;
+            return;
+        }
+        // find the position in the list to insert the new member
+        int index = 0;
+        for (index = 0; index < Number; index++)
+        {
+            Console.WriteLine($"Comparing {member.ToString()} to {members[index].ToString()}");
+            // if the member is the same as the current member, don't insert it
+            if (member.CompareTo(members[index]) == 0)
+            {
+                Console.WriteLine("Member already exists in collection");
+                return;
+            }
+            // if the member is less than the current member, insert it here
+            else if (member.CompareTo(members[index]) < 0)
+            {
+                Console.WriteLine($"Inserting {member.ToString()} at index " + index);
+                break;
+            }
+        }
+        // shift all the members to the right of where the new member is meant to go
+        for (int i = Number; i > index; i--)
+        {
+            members[i] = members[i - 1];
+        }
+        // insert the member in the new free position
+        members[index] = (Member)member;
+        // update the count
+        count++;
+    }
+
+    // Remove a given member out of this member collection
+    // Pre-condition: nil
+    // Post-condition: the given member is removed from this member collection, if the given member is in this member collection and the members in this member collection remains sorted in alphabetical order by their full names; otherwise, no member is removed from this member collection and this member collection remains unchanged. 
+    public void Delete(string lastname, string firstname)
+    {
+        // search through the collection for the member
+        for (int index = 0; index < Number; index++)
+        {
+            // if the member is found, remember the index
+            if (members[index].LastName == lastname && members[index].FirstName == firstname)
+            {
+                // shift every value to the right of that member to the left
+                // this essentially overwrites the value of the member to be removed
+                // while keeping the list in alphabetical order
+                for (int j = index; j < Number - 1; j++)
+                {
+                    members[j] = members[j + 1];
+                }
+                count -= 1;
+                members[Number] = null; // remove the final duplicate member
+                break; // break so we don't loop again                
+            }
+        }
+    }
+
+    // Search a given member by last name and first name in this member collection 
+    // Pre-condition: nil
+    // Post-condition: return true if the given member is in this member collection; return false otherwise. This member collection remains unchanged.
+    public bool Search(string lastname, string firstname)
+    {
+        // search through the collection for the member
+        for (int index = 0; index < Number; index++)
+        {
+            // if the member is found, return true
+            if (members[index].LastName == lastname && members[index].FirstName == firstname)
+            {
+                return true;
+            }
+        }
+        // otherwise, return false
+        return false;
+    }
+
+    // Remove all the members in this member collection
+    // Pre-condition: nil
+    // Post-condition: Capacity remins unchanged; Number = 0; this member collection is empty.
+    public void Clear()
+    {
+        count = 0;
+        members = new Member[capacity];
+    }
+
+    // Return a string containing the information about all the members in this member collection.
+    // The information includes last name, first name and contact number in this order
+    // Pre-condition: nil
+    // Post-condition: a string containing the information about all the members in this member collection is returned, the order of the members in the returned string is the same with that in this member collection and this collection remains unchanged. 
+    //                  The information about a member includes the last name, first name and contact phone number of the member, which are separated by a comma (no whitespace before or after the comma). The members are separated by a semicolon (no white space before or after the semicolon).
+    public override string ToString()
+    {
+        string returnValue = "";
+        for (int index = 0; index < Number; index++)
+        {
+            Member current = members[index];
+            returnValue += $"{current.LastName},{current.FirstName},{current.ContactNumber};";
+        }
+        return returnValue;
+    }
+}
